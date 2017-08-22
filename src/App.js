fc3bf03120afe477b0d130c4330372a5fc4b50ba
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2017-08-22 14:58:10 -0400

    add checkpoint 00 to weather

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Weather from "./weather/checkpoint_04/weather.js";
+import Weather from "./weather/checkpoint_00/weather.js";
 import Mondrian from "./mondrian/checkpoint_01";
-import SmarterWeather from "./smarter-weather/checkpoint_04/weather.js";
+import SmarterWeather from "./smarter-weather/checkpoint_01/weather.js";
 
-export default SmarterWeather;
+export default Weather;
